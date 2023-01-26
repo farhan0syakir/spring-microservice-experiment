87904d672580a931be674049ea90ece0ceb91d28
farhan.syakir <farhan0syakir@gmail.com>
2023-01-26 21:15:56 +0200

    multiple project

---
+++ recipe-list/src/main/java/com/myrecipe/recipedetail/RecipeDetailApplication.java
@@ -0,0 +1,13 @@
+package com.myrecipe.recipedetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeDetailApplication.class, args);
+	}
+
+}
