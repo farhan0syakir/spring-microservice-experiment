0c137e842d5ef06af3a483658b5e57a539e5d3ec
farhan.syakir <farhan0syakir@gmail.com>
2023-02-06 16:26:27 +0200

    add port return

--- recipe-detail/src/main/java/com/myrecipe/recipedetail/RecipeDetailController.java
+++ recipe-detail/src/main/java/com/myrecipe/recipedetail/RecipeDetailController.java
@@ -1,11 +1,15 @@
 package com.myrecipe.recipedetail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecipeDetailController {
+
+	@Value("${server.port}")
+	private String serverPort;
 
 	@GetMapping("recipe-detail/{id}")
 	public RecipeDetail getRecipeDetail(@PathVariable String id) {
@@ -17,6 +21,6 @@ public class RecipeDetailController {
 
 	@GetMapping("hello")
 	public String hello() {
-		return "greetings";
+		return "greetings from " + serverPort;
 	}
 }
